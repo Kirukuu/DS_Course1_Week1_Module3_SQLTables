b5322c20560b1021114e2a9345eafcdc6c547967
Kirukuu <kirukuwaweru@gmail.com>
2026-05-07 05:52:59 +0300

    Completed LAB

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ pd.read_sql("""SELECT * FROM sqlite_master""", conn)
 
 # CodeGrade step1
 df_boston = pd.read_sql("""
-    SELECT e.firstName, e.lastName, e.jobTitle
+    SELECT e.firstName || ' ' || e.lastName AS name, e.jobTitle
     FROM employees e
     JOIN offices o ON e.officeCode = o.officeCode
     WHERE o.city = 'Boston'
 """, conn)
+# 
+# 
+# 
+# pd.read_sql("""
+#     SELECT e.firstName, e.lastName, e.jobTitle
+#     FROM employees e
+#     JOIN offices o ON e.officeCode = o.officeCode
+#     WHERE o.city = 'Boston'
+# """, conn)
 
 # CodeGrade step2
 df_zero_emp = pd.read_sql("""
